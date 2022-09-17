b3000956fec6922f442d4ed99c84584ca866244a
Meetmakwana7396 <makwnameet7301@gmail.com>
2022-09-17 10:39:29 +0530

    card prototype

--- Q2/Client/html/index.php
+++ Q2/Client/html/index.php
@@ -20,7 +20,24 @@
                 <p>BOOKSTOP</p>
                 <p class="sent">a place to find peace of your mind</p>
             </div>
-            
+            <div class="card-area area">
+                <h3>Book List</h3>
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Book title</h5>
+                                <p class="card-text"> <span class="card-labels">Category: </span> Lorem, ipsum. </p>
+                                <p class="card-text"> <span class="card-labels">Description: </span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, deserunt? </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
